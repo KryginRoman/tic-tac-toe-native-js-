aaec7565bd082280a936c20e4619ab1efb6d0aa7
Roman <kenix1337@yandex.ru>
2019-11-22 00:47:23 +0400

    winner verification code improved, player authorization added

--- script/script.js
+++ script/script.js
@@ -3,6 +3,15 @@ class PlayingField {
         ceil.textContent = symbol;
         ceil.classList.add("field__ceil_filled");
     }
+    drawPlayerName() {
+        const playerBlock = document.createElement("div");
+        const namePlace = document.createElement("span")
+        playerBlock.classList.add("field__players");
+        playerBlock.textContent = "Current player : ";
+        playerBlock.append(namePlace);
+
+        return playerBlock;
+    }
     drawRow() {
         const row = document.createElement("div");
         row.classList.add("field__row");
@@ -18,7 +27,8 @@ class PlayingField {
     drawField() {
         this.field = document.createElement("div");
         this.field.classList.add("field");
-        
+        this.field.append(this.drawPlayerName());
+
         for (let i = 0; i < 3; i++) {
             this.field.append(this.drawRow());
         }
@@ -29,19 +39,18 @@ class PlayingField {
   
 class TicTacToe {
     constructor(player1, player2) {
-        this.playField = new PlayingField();
         this.symbolsOfMoves = ["X", "0"];
-        this.firstPlayerName = player1;
-        this.secondPlayerName = player2;
-        this.currentSymbol = 0;
+        this.playerNames = [player1, player2];
+        this.currentOrder = 0;
     }
     endGame() {
         [...this.playField.field.querySelectorAll(".field__ceil")]
             .forEach(c => c.removeEventListener("click", this.stepHandler));
+
         console.log("game over");
     }
     checkWinners() {
-    const winnersComb = [
+        const winnersComb = [
             [0, 1, 2],
             [3, 4, 5],
             [6, 7, 8],
@@ -51,34 +60,78 @@ class TicTacToe {
             [0, 4, 8],
             [2, 4, 6]
         ];
-      
-    for (let i = 0; i < winnersComb.length; i++) {
-        const allCeils = this.playField.field.querySelectorAll(".field__ceil");
-        const ceilsText = [allCeils[winnersComb[i][0]].textContent, 
-                    allCeils[winnersComb[i][1]].textContent, 
-                    allCeils[winnersComb[i][2]].textContent
-                    ];
-        const ceils = [...ceilsText].every(item => item === ceilsText[0] && item != "");
         
-        if (ceils) {
-            allCeils[winnersComb[i][0]].classList.add("field__ceil_win");
-            allCeils[winnersComb[i][1]].classList.add("field__ceil_win");
-            allCeils[winnersComb[i][2]].classList.add("field__ceil_win");
-            this.endGame();
-            break;
-        }
-      }
+        winnersComb
+            .forEach((item) => {
+                const ceils = this.playField.field.querySelectorAll(".field__ceil");
+                const ceilTxt = item.map((item) => ceils[item].textContent);
+
+                if (ceilTxt.every(item => item === ceilTxt[0] && item != "")) {
+                    item.map((item) => ceils[item])
+                        .forEach(ceil => ceil.classList.add("field__ceil_win"));
+                    this.endGame();
+                }
+            });
+    }
+    changeNamePlayer() {
+        this.playField.field
+            .querySelector(".field__players > span")
+            .textContent = this.playerNames[this.currentOrder];
     }
     makeStep(ceil) {
-        this.playField.fillCell(ceil, this.symbolsOfMoves[this.currentSymbol]);
-        this.currentSymbol >= 1 ? this.currentSymbol = 0 : this.currentSymbol++;
+        this.playField.fillCell(ceil, this.symbolsOfMoves[this.currentOrder]);
+        this.changeNamePlayer();
+        this.currentOrder >= 1 ? this.currentOrder = 0 : this.currentOrder++;
         this.checkWinners();
     }
     startGame() {
-        const filed = this.playField.drawField();
+        this.playField = new PlayingField();
+        this.playField.drawField();
+        this.changeNamePlayer();
         this.stepHandler = ({target}) => this.makeStep(target);
-        [...filed.querySelectorAll(".field__ceil")]
+        [...this.playField.field.querySelectorAll(".field__ceil")]
             .forEach(i => i.addEventListener("click", this.stepHandler, {"once": true}));
-        return filed;
+        return this.playField.field;
     }
 }
+
+class Aauthorization {
+    createAuthorizationModal() {
+        this.modal = document.createElement("div");
+        const modalInner = `
+            <div class="field__authorization-input">
+                <input type="text" placeholder="First player name">
+            </div>
+            <div class="field__authorization-input">
+                <input type="text" placeholder="Second player name">
+            </div>
+            <button type="button" class="field__authorization-button">Let's Play</button>
+        `;
+        this.modal.classList.add("field__authorization");
+        this.modal.innerHTML = modalInner;
+    }
+    checkInputs(inputValues) {
+        return inputValues.filter(v => (/[A-Za-z0-9_-]{1,8}/.test(v))).length > 1;
+    }
+    startAuthorization() {
+        this.createAuthorizationModal();
+        this.modal.querySelector(".field__authorization-button")
+            .addEventListener("click", () => {
+                const playerNames = [...this.modal.querySelectorAll("input")].map(item => item.value);
+
+                if (!this.checkInputs(playerNames)) {
+                    console.log("invalid name");
+                    return false;
+                }
+
+                const playSession = new TicTacToe(playerNames[0], playerNames[1]).startGame();
+
+                document.querySelector(".field-wrap").append(playSession);
+                this.modal.remove();
+            });
+        
+        document.body.append(this.modal);
+    }
+}
+
+new Aauthorization().startAuthorization();
